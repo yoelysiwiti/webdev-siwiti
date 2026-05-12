7414a6b0cf510653f44c00f425201d48a540bae6
yoelysiwiti2 <sweetB@2025>
2026-05-12 22:43:40 +0300

    new function added

--- app.py
+++ app.py
@@ -2,8 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
-print("Siwiti, this is app.py")
-
 @app.route("/")
+
+def create_form():
+    print("My name is yoeli siwiti, the king of the jungle")
+
 def home():
+    create_form()
     return "Hello from Render Flask app!"
+home()
