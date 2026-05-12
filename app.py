14b44e7a4a9531a822b065b3886e9064b45c0910
yoelysiwiti2 <sweetB@2025>
2026-05-12 23:33:44 +0300

    direct

--- app.py
+++ app.py
@@ -5,18 +5,5 @@ app = Flask(__name__)
 print("Siwiti, this is app.py")
 
 @app.route("/")
-
-<<<<<<< HEAD
-
 def home():
-    message =  "Hello from Render Flask app, the messages variable declaredSS";
-    return message;
-=======
-def call_message();
-    return "Hello from Render Flask app, the messages from call message()";
-    
-def home():
-    call_message();
-    return message;
-
->>>>>>> aa5bfb3f3f4d327ed4493420f44066d6421ecfe9
+    return "Hello from Render Flask app!"
